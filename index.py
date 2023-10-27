85b3f94b31088f76430f3ec5e5cdd6c91563be6b
Nakanishi <ponzxp@gmail.com>
2023-10-28 00:35:38 +0900

    push button -> show image

---
+++ index.py
@@ -0,0 +1,13 @@
+import cv2
+
+# read image
+image = cv2.imread('output.jpg')
+
+# show image
+cv2.imshow("Image Show", image)
+
+# wait while push anykey(without this, finish program soon)
+cv2.waitKey(0)
+
+# finish all window
+cv2.destroyAllWindows()
